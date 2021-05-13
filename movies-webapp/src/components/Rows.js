e6de345e6bda8dac692ec9b4576d84109f899fec
Abhay5855 <ap3919529@gmail.com>
2021-05-13 22:55:00 +0530

    docs: fetch the conditon as props and give poster size according to the condition

--- movies-webapp/src/components/Rows.js
+++ movies-webapp/src/components/Rows.js
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Rows({title, fetchUrl }) {
+function Rows({title, fetchUrl , isImageBigger}) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -27,13 +27,13 @@ function Rows({title, fetchUrl }) {
   return (
     <div className="row">
 
-        <h2>{title}</h2>
-      <div className="row__poster">
+       <h2 className="row__heading">{title}</h2>
+      <div className= "row__poster">
         {movies.map((movie) => {
           return (
-            <img className="row__images"
+            <img className={`row__images ${isImageBigger && "row__imagesLarger"}`}
               key={movie.id}
-              src={`${base_url}${movie.poster_path}`}
+              src={`${base_url}${isImageBigger? movie.poster_path: movie.backdrop_path}`}
               alt={movie.name}
             ></img>
           );
