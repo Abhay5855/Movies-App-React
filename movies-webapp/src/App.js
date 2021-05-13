663c0e21606fee88e9c0cb9d20b8f265e2fd2442
Abhay5855 <ap3919529@gmail.com>
2021-05-13 09:53:53 +0530

    docs: Importing the Input component and using state

--- movies-webapp/src/App.js
+++ movies-webapp/src/App.js
@@ -1,23 +1,25 @@
 import "./App.css";
-import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Rows from "./components/Rows";
-import Navbar from "./components/Navbar"
+// import Navbar from "./components/Navbar";
+// import Add from "./components/Add";
+import Input from "./components/Input"
 
 import { useState, useEffect } from "react";
 
 function App() {
-  const [movies, setMovies] = useState([
-   
-  ]);
+  const [movies, setMovies] = useState([]);
+
+  const [searchMovies, setSearchMovies] = useState('');
+
+  console.log(searchMovies);
 
   const getMovies = async () => {
-    const url = "http://www.omdbapi.com/?s=avengers&apikey=e86e479e";
+    const url = "http://www.omdbapi.com/?s='rocky'&apikey=e86e479e";
 
     const response = await fetch(url);
 
     const responseJson = await response.json();
-
-    
 
     setMovies(responseJson.Search);
   };
@@ -29,67 +31,14 @@ function App() {
   return (
     <>
 
+            <Input searchMovies={searchMovies}  setSearchMovies={setSearchMovies}  />
+           
+            <Rows  movies={movies} />
+         
+    </>
+  );
 
-     <Router>
-
-        <Navbar />
-
-
-
-       <Switch>
-         <Route exact path="/" >
-           <Watchlist />
-         </Route>
-
-         <Route path="/add">
-
-         <Add />
-
-
-         </Route>
-
-         <Route>
-           <Watched />
-         </Route>
-       </Switch>
-
-
-     </Router>
-
-
-
-
-
-      {/* <div className="movie__container">
-        <Rows key={movies.imdbID} movies={movies} />
-        </div> */}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        </>
-
-
-
-
-
-        )
 
 }
-
 
 export default App;
