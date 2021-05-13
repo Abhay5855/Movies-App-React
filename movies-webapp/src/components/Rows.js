a995001c80dc38123efb37700c4a21fc0633a33d
Abhay5855 <ap3919529@gmail.com>
2021-05-13 13:44:58 +0530

    docs: fetch the url and map the movies through image

--- movies-webapp/src/components/Rows.js
+++ movies-webapp/src/components/Rows.js
@@ -1,12 +1,44 @@
 import React from "react";
 import "./Rows.css";
-function Rows(props) {
+import axios from "../axios";
+
+import { useState, useEffect } from "react";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Rows({title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+
+      setMovies(request.data.results);
+
+      return request;
+    }
+
+    fetchData();
+  },[fetchUrl]);
+
   return (
     <div className="row">
+
+        <h2>{title}</h2>
       <div className="row__poster">
-        {props.movies.map((movie, idx) => (
-          <img className="row__images" src={movie.Poster} alt=""></img>
-        ))}
+        {movies.map((movie) => {
+          return (
+            <img className="row__images"
+              key={movie.id}
+              src={`${base_url}${movie.poster_path}`}
+              alt={movie.name}
+            ></img>
+          );
+        })}
+
+        {/* {props.movies.map((movie) => (
+          <img className="row__images" src={movie.Poster} alt="movie"></img>
+        ))} */}
       </div>
     </div>
   );
