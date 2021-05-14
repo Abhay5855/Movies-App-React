dd998aa7cf320842b5b305ed6db95b3c44233698
Abhay5855 <ap3919529@gmail.com>
2021-05-14 12:03:12 +0530

    feat: Displaying the movie trailer using the urlParameters

--- movies-webapp/src/components/Rows.js
+++ movies-webapp/src/components/Rows.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./Rows.css";
 import axios from "../axios";
+import Youtube from "react-youtube"
+import movieTrailer from "movie-trailer"
 
 import { useState, useEffect } from "react";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Rows({title, fetchUrl , isImageBigger}) {
+function Rows({ title, fetchUrl, isImageBigger }) {
   const [movies, setMovies] = useState([]);
+
+  const [trailerUrl, setTrailerUrl] = useState('');
 
   useEffect(() => {
     async function fetchData() {
@@ -19,27 +23,58 @@ function Rows({title, fetchUrl , isImageBigger}) {
     }
 
     fetchData();
-  },[fetchUrl]);
+  }, [fetchUrl]);
 
+
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+
+    };
+
+
+    const handleClick = (movie) => {
+      if(trailerUrl){
+        setTrailerUrl('')
+      }else {
+
+        movieTrailer(movie?.name || '')
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search)
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error))
+      }
+    }
 
   
+
+ 
+
   return (
     <div className="row">
-
-       <h2 className="row__heading">{title}</h2>
-      <div className= "row__poster">
+      <h2 className="row__heading">{title}</h2>
+      <div className="row__poster">
         {movies.map((movie) => {
           return (
-            <img className={`row__images ${isImageBigger && "row__imagesLarger"}`}
+            <img
+              onClick={()=>handleClick(movie)}
+              className={`row__images ${isImageBigger && "row__imagesLarger"}`}
               key={movie.id}
-              src={`${base_url}${isImageBigger? movie.poster_path: movie.backdrop_path}`}
+              src={`${base_url}${
+                isImageBigger ? movie.poster_path : movie.backdrop_path
+              }`}
               alt={movie.name}
             ></img>
           );
         })}
-
-       
       </div>
+
+      { trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
